a2738e0fbe8240f2786ffb79bb6d9f8b2a00b768
Luke Arms <luke@linacreative.com>
2019-06-23 18:04:58 +1000

    Fix wrong distro-info package name in ubuntu/system-install.sh

--- ubuntu/system-install.sh
+++ ubuntu/system-install.sh
@@ -14,8 +14,8 @@ assert_not_root
 
 console_message "Upgrading everything that's currently installed..." "" $BLUE
 
-sudo apt-get update || exit 1
+sudo apt-get -qq update || exit 1
 sudo apt-get -y dist-upgrade || exit 1
 [ "$IS_SNAP_INSTALLED" -eq "1" ] && { sudo snap refresh || exit 1; }
 
-apt_force_install_packages "aptitude software-properties-common ubuntu-distro-info whiptail"
+apt_force_install_packages "aptitude software-properties-common distro-info whiptail"
