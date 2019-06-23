6d6141d8e5ba3cfade3fd868cbff54bae14c02c8
Luke Arms <luke@linacreative.com>
2019-06-24 02:29:49 +1000

    Implement PPA triggers

--- ubuntu/system-install.sh
+++ ubuntu/system-install.sh
@@ -12,6 +12,15 @@ assert_not_root
 # TODO
 #export DEBIAN_FRONTEND=noninteractive
 
+console_message "Upgrading everything that's currently installed..." "" $BLUE
+
+sudo apt-get -qq update && APT_CACHE_DIRTY=0 || exit 1
+sudo apt-get -y dist-upgrade || exit 1
+[ "$IS_SNAP_INSTALLED" -eq "1" ] && { sudo snap refresh || exit 1; }
+
+# install prequisites and packages that may be needed to bootstrap others
+apt_force_install_packages "apt-transport-https aptitude ca-certificates curl distro-info gnupg-agent software-properties-common software-properties-common whiptail"
+
 # ensure all of Ubuntu's repositories are available (including "proposed" archives)
 apt_enable_ubuntu_repository main "proposed"
 apt_enable_ubuntu_repository restricted "proposed"
@@ -30,15 +39,6 @@ EOF
 
 fi
 
-console_message "Upgrading everything that's currently installed..." "" $BLUE
-
-sudo apt-get -qq update && APT_CACHE_DIRTY=0 || exit 1
-sudo apt-get -y dist-upgrade || exit 1
-[ "$IS_SNAP_INSTALLED" -eq "1" ] && { sudo snap refresh || exit 1; }
-
-# install prequisites and packages that may be needed to bootstrap others
-apt_force_install_packages "apt-transport-https aptitude ca-certificates curl distro-info gnupg-agent software-properties-common software-properties-common whiptail"
-
 # register PPAs (note: this doesn't add them to the system straightaway; they are added on-demand if/when the relevant packages are actually installed)
 apt_register_ppa "caffeine-developers/ppa" "caffeine"
 apt_register_ppa "eosrei/fonts" "fonts-twemoji-svginot"
@@ -55,8 +55,15 @@ apt_register_repository docker "https://download.docker.com/linux/ubuntu/gpg" "d
 apt_register_repository google-chrome "https://dl.google.com/linux/linux_signing_key.pub" "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" "google-chrome-stable"
 apt_register_repository microsoft "https://packages.microsoft.com/keys/microsoft.asc" "deb [arch=amd64] https://packages.microsoft.com/ubuntu/$DISTRIB_RELEASE/prod $DISTRIB_CODENAME main" "powershell*"
 apt_register_repository mkvtoolnix "https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt" "deb https://mkvtoolnix.download/ubuntu/ $DISTRIB_CODENAME main" "mkvtoolnix mkvtoolnix-gui"
+apt_register_repository nodesource "https://deb.nodesource.com/gpgkey/nodesource.gpg.key" "deb https://deb.nodesource.com/node_8.x $DISTRIB_CODENAME main" "nodejs"
 apt_register_repository owncloud-client "https://download.opensuse.org/repositories/isv:ownCloud:desktop/Ubuntu_$DISTRIB_RELEASE/Release.key" "deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_$DISTRIB_RELEASE/ /" "owncloud-client"
 apt_register_repository sublime-text "https://download.sublimetext.com/sublimehq-pub.gpg" "deb https://download.sublimetext.com/ apt/stable/" "sublime-text"
 apt_register_repository typora "https://typora.io/linux/public-key.asc" "deb https://typora.io/linux ./" "typora"
 apt_register_repository virtualbox "https://www.virtualbox.org/download/oracle_vbox_2016.asc" "deb https://download.virtualbox.org/virtualbox/debian $DISTRIB_CODENAME contrib" "virtualbox-*"
+apt_register_repository yarn "https://dl.yarnpkg.com/debian/pubkey.gpg" "deb https://dl.yarnpkg.com/debian/ stable main" "yarn"
+
+# ok, time to get underway
+apt_install_packages "Docker Community Edition" "docker-ce docker-ce-cli containerd.io" Y Y
+
+apt_process_queue
 
