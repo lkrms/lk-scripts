82183091706ad82e53328dcea4ba0d2006eae631
Luke Arms <luke@linacreative.com>
2019-08-05 15:53:33 +1000

    Fix bugs in package state detection, add HWE removal

--- ubuntu/dev-system-install.sh
+++ ubuntu/dev-system-install.sh
@@ -19,6 +19,8 @@ assert_is_ubuntu
 assert_not_server
 assert_not_root
 
+apt_mark_cache_clean
+
 APT_GUI_PACKAGES="\
 autokey-common
 autokey-gtk
@@ -456,7 +458,6 @@ if [ "${#SNAPS_INSTALL[@]}" -gt "0" ]; then
 
 fi
 
-apt_upgrade_all
 apt_process_queue
 
 # critical post-apt tasks
@@ -519,7 +520,17 @@ fi
 # workaround for bugs introduced in libx11-6 2:1.6.4-3ubuntu0.2
 HOLD_PACKAGES=(libx11-6 libx11-data libx11-dev libx11-doc libx11-xcb-dev libx11-xcb1)
 
-if dpkg-query -f '${Version}\n' -W "${HOLD_PACKAGES[@]}" | grep -Eq "$(sed_escape_search "2:1.6.4-3ubuntu0.2")"; then
+for i in "${!HOLD_PACKAGES[@]}"; do
+
+    if ! apt_package_installed "${HOLD_PACKAGES[$i]}"; then
+
+        unset "HOLD_PACKAGES[$i]"
+
+    fi
+
+done
+
+if [ "${#HOLD_PACKAGES[@]}" -gt "0" ] && dpkg-query -f '${Version}\n' -W "${HOLD_PACKAGES[@]}" | grep -Eq "$(sed_escape_search "2:1.6.4-3ubuntu0.2")"; then
 
     VERSIONED_HOLD_PACKAGES=()
 
@@ -537,6 +548,8 @@ if dpkg-query -f '${Version}\n' -W "${HOLD_PACKAGES[@]}" | grep -Eq "$(sed_escap
 
 fi
 
+apt_upgrade_all
+
 ALL_PACKAGES=($(printf '%s\n' "${APT_INSTALLED[@]}" | sort | uniq))
 console_message "${#ALL_PACKAGES[@]} installed $(single_or_plural ${#ALL_PACKAGES[@]} "package is" "packages are") managed by $(basename "$0"):" "" "$BLUE"
 COLUMNS="$(tput cols)"
@@ -547,4 +560,69 @@ if apt_package_available "linux-generic-hwe-$DISTRIB_RELEASE" && apt_package_ava
     echo
     console_message "To use the Ubuntu LTS enablement stack, but only for X server, run:" "sudo apt-get install linux-generic-hwe-${DISTRIB_RELEASE}- xserver-xorg-hwe-$DISTRIB_RELEASE" "$BOLD" "$CYAN"
 
+elif apt_package_installed "linux-generic-hwe-$DISTRIB_RELEASE"; then
+
+    echo
+    if get_confirmation "Ubuntu LTS enablement package ${BOLD}linux-generic-hwe-${DISTRIB_RELEASE}${RESET} is currently installed. Remove it and any related kernel packages?" N Y; then
+
+        apt_remove_packages "linux-generic-hwe-$DISTRIB_RELEASE" "linux-image-generic-hwe-$DISTRIB_RELEASE" "linux-headers-generic-hwe-18.04-$DISTRIB_RELEASE"
+
+        apt_install_packages "kernel" "linux-generic" N N
+
+        apt_process_queue
+
+    fi
+
 fi
+
+CURRENT_KERNEL=($(apt-cache depends --recurse --no-recommends --no-suggests --no-conflicts --no-breaks --no-replaces --no-enhances linux-generic | grep -E '^linux-image-[-.0-9]+-generic$'))
+
+if [ "${#CURRENT_KERNEL[@]}" -eq "1" ] && apt_package_installed "${CURRENT_KERNEL[0]}"; then
+
+    CURRENT_KERNEL_VERSION="$(dpkg-query -f '${Version}\n' -W "${CURRENT_KERNEL[0]}")"
+
+    OTHER_KERNEL_PACKAGES=()
+
+    IFS=$'\t'
+
+    while IFS= read -r LINE; do
+
+        PACKAGE=($LINE)
+
+        if dpkg --compare-versions "${PACKAGE[1]}" gt "$CURRENT_KERNEL_VERSION"; then
+
+            OTHER_KERNEL_PACKAGES+=("${PACKAGE[0]}")
+
+        fi
+
+    done < <(dpkg-query -f '${binary:Package}\t${Version}\t${db:Status-Status}\n' -W "linux-image-*-generic" "linux-modules-*-generic" "linux-headers-*" | grep $'\tinstalled$' | cut -d $'\t' -f1-2 | grep -v $'^linux-headers-generic\t')
+
+    unset IFS
+
+    if [ "${#OTHER_KERNEL_PACKAGES[@]}" -gt "0" ]; then
+
+        console_message "Most recent kernel provided by the ${BOLD}linux-generic${RESET} package:" "$CURRENT_KERNEL_VERSION" "$CYAN"
+
+        console_message "${#OTHER_KERNEL_PACKAGES[@]} kernel $(single_or_plural "${#OTHER_KERNEL_PACKAGES[@]}" package packages) to delete:" "${OTHER_KERNEL_PACKAGES[*]}" "$BOLD" "$YELLOW"
+
+        if get_confirmation "Delete the kernel $(single_or_plural "${#OTHER_KERNEL_PACKAGES[@]}" package packages) listed above?" N Y; then
+
+            sudo debconf-set-selections <<EOF
+linux-base linux-base/removing-running-kernel boolean false
+EOF
+
+            apt_remove_packages "${OTHER_KERNEL_PACKAGES[@]}"
+
+            apt_process_queue
+
+        fi
+
+    fi
+
+else
+
+    console_message "Unable to identify most recent kernel provided by the ${BOLD}linux-generic${RESET} package" "" "$BOLD" "$RED"
+
+fi
+
+sudo apt-get "${APT_GET_OPTIONS[@]}" autoremove
