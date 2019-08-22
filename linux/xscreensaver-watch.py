0965738435352e3d7424ab16956d1c83894139eb
Luke Arms <luke@linacreative.com>
2019-08-22 13:22:02 +1000

    Add explanation to xscreensaver-watch.py

--- linux/xscreensaver-watch.py
+++ linux/xscreensaver-watch.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+"""
+Because ``xscreensaver`` doesn't emit ``dbus`` signals, applications that
+rely on them for session lock/unlock notifications (e.g. KeePassXC) don't work
+correctly. To emit ``org.freedesktop.login1.Session.Lock`` and
+``org.freedesktop.login1.Session.Unlock`` signals based on ``xscreensaver``'s
+state changes, add this script to your autostart, or create a systemd service
+for it.
+"""
+
 import atexit
 import dbus
 import os
@@ -8,7 +17,8 @@ import signal
 import subprocess
 import sys
 
-assert "XDG_SESSION_ID" in os.environ, "Error: XDG_SESSION_ID not set"
+assert "XDG_SESSION_ID" in os.environ, \
+    "Error: environment variable XDG_SESSION_ID is not set"
 
 bus = dbus.SystemBus()
 o = bus.get_object("org.freedesktop.login1",
