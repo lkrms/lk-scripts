9b224301b5b47a0c02b53176991b462be8e3cead
Luke Arms <luke@linacreative.com>
2019-08-29 10:59:44 +1000

    Add support for xinput-settings (i.e. not just natural scrolling)

--- linux/xinput-load.sh
+++ linux/xinput-load.sh
@@ -90,3 +90,38 @@ if [ "${#FILE_TO_ARRAY[@]}" -gt "0" ]; then
     fi
 
 fi
+
+file_to_array "$CONFIG_DIR/xinput-settings" '^[[:space:]]*$' '^#'
+
+if [ "${#FILE_TO_ARRAY[@]}" -gt "0" ]; then
+
+    # each setting is a two-line tuple: regex, then setting
+    for ((i = 0; i < ${#FILE_TO_ARRAY[@]}; i++)); do
+
+        PATTERN="${FILE_TO_ARRAY[$i]}"
+
+        ((++i))
+        ((i < ${#FILE_TO_ARRAY[@]})) || {
+            ((PATTERN_ID = (i + 1) / 2))
+            echo -e "Error: no setting for pattern $PATTERN_ID in $FILE_TO_ARRAY_FILENAME\n" >&2
+            break
+        }
+
+        SETTING="${FILE_TO_ARRAY[$i]}"
+
+        LINES=("$(xinput list | grep -E "$PATTERN")")
+        DEVICE_IDS=($(printf '%s\n' "${LINES[@]}" | sort | uniq | gnu_grep -Po '(?<=\bid=)[0-9]+\b'))
+
+        if [ "${#DEVICE_IDS[@]}" -gt "0" ]; then
+
+            for DEVICE_ID in "${DEVICE_IDS[@]}"; do
+
+                eval xinput_set_prop "$DEVICE_ID" "$SETTING" || true
+
+            done
+
+        fi
+
+    done
+
+fi
