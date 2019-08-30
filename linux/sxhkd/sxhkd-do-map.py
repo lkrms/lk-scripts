d40ba29e1afa49eae722e0fb57563e7d56e8505e
Luke Arms <luke@linacreative.com>
2019-08-30 22:52:21 +1000

    Fix sxhkd-do-map.py to properly support Super+Click

--- linux/sxhkd/sxhkd-do-map.py
+++ linux/sxhkd/sxhkd-do-map.py
@@ -6,6 +6,33 @@ import re
 import subprocess
 import sys
 
+
+def make_xte_commands(command, keys, both_modifiers=False):
+
+    xte_dictionary = {
+        "ctrl": ["Control_L", "Control_R"],
+        "alt": ["Alt_L", "Alt_R"],
+        "super": ["Super_L", "Super_R"],
+        "shift": ["Shift_L", "Shift_R"],
+    }
+
+    commands = []
+
+    for key in keys:
+
+        if key in xte_dictionary.keys():
+            key_array = xte_dictionary[key] if both_modifiers else [
+                xte_dictionary[key][0]]
+        else:
+            key_array = [key]
+
+        for the_key in key_array:
+            commands.append("{0} {1}".format(command, the_key))
+            commands.append("usleep 2000")
+
+    return commands
+
+
 log_directory = os.path.normpath(os.path.dirname(
     os.path.realpath(__file__)) + "/../../log")
 log_file = os.path.join(log_directory, "sxhkd-do-map.py.log")
@@ -13,7 +40,7 @@ log_file = os.path.join(log_directory, "sxhkd-do-map.py.log")
 if not os.path.exists(log_directory):
     os.makedirs(log_directory)
 logging.basicConfig(
-    filename=log_file, format="%(relativeCreated)s %(levelname)s: %(message)s", level=logging.INFO)
+    filename=log_file, format="%(asctime)s %(relativeCreated)s %(levelname)s: %(message)s", level=logging.INFO)
 
 logging.info("keystroke in: {0}".format(sys.argv[1]))
 
@@ -51,6 +78,7 @@ in_alt = "alt" in args
 in_super = "super" in args
 in_shift = "shift" in args
 
+# make things macOS-like by default
 if not skip:
     out_ctrl = in_super
     out_alt = in_alt
@@ -68,15 +96,22 @@ original_key = key
 is_alpha = re.search(r"^[a-z]$", key)
 is_num = re.search(r"^[0-9]$", key)
 is_click = re.search(r"^button[0-9]+$", key)
+mouse_button = re.sub(r"^button", "", key) if is_click else ""
 is_command = in_super and not (in_ctrl or in_alt)
 is_command_option = in_super and in_alt and not in_ctrl
 
-keyup_pre_command = []
+keyup_pre_command = [] if is_click else [key]
 key_command = []
-mouseup_pre_command = []
+
+mouseup_pre_command = mouse_button if is_click else ""
 click_key_command = []
-click_command = []
-clear_modifiers_manually = False
+click_command = ""
+
+key = "" if is_click else key
+
+clear_modifiers = True
+use_xte = False
+
 done = False
 
 if not skip:
@@ -91,27 +126,17 @@ if not skip:
 
         # developer tools
         if is_command_option and not in_shift and original_key == "i":
-            keyup_pre_command = [original_key]
             key_command = ["ctrl", "shift", "j"]
             done = True
 
         # bookmarks manager
         if is_command_option and not in_shift and original_key == "b":
-            keyup_pre_command = [original_key]
             key_command = ["ctrl", "shift", "o"]
             done = True
-
-    if is_click:
-
-        click_command = [re.sub(r"^button", "", original_key)]
-        mouseup_pre_command = click_command
-        clear_modifiers_manually = True
-        key = ""
 
     if quit_alt_f4:
 
         if is_command and not in_shift and original_key == "q":
-            keyup_pre_command = [original_key]
             key_command = ["alt", "F4"]
             done = True
 
@@ -125,7 +150,6 @@ if not skip:
                 key = "y"
 
             if original_key == "g":
-                keyup_pre_command = [original_key]
                 done = True
 
                 # "find next"
@@ -137,60 +161,89 @@ if not skip:
                     key_command = ["shift", "F3"]
 
 if not done:
-    if out_ctrl and (not clear_modifiers_manually or not in_ctrl):
+    if out_ctrl and (clear_modifiers or not in_ctrl):
         key_command.append("ctrl")
-    if out_alt and (not clear_modifiers_manually or not in_alt):
+    if out_alt and (clear_modifiers or not in_alt):
         key_command.append("alt")
-    if out_super and (not clear_modifiers_manually or not in_super):
+    if out_super and (clear_modifiers or not in_super):
         key_command.append("super")
-    if out_shift and (not clear_modifiers_manually or not in_shift):
+    if out_shift and (clear_modifiers or not in_shift):
         key_command.append("shift")
     if key:
-        keyup_pre_command.append(original_key)
         key_command.append(key)
-    if click_command:
+    if mouse_button:
+        click_command = mouse_button
         click_key_command = key_command
         key_command = []
 
-modifiers = []
-command = ["xdotool"]
+# xdotool's modifier handling is a bit broken
+use_xte = use_xte or (clear_modifiers and (click_command or click_key_command))
 
-if keyup_pre_command:
-    command.extend(["keyup", "--delay", "0", "+".join(keyup_pre_command)])
+command = ["xte"] if use_xte else ["xdotool"]
 
-if mouseup_pre_command:
-    command.extend(["mouseup", "+".join(mouseup_pre_command)])
+if use_xte:
 
-if clear_modifiers_manually:
-    if in_ctrl and not out_ctrl:
-        modifiers.append("ctrl")
-    if in_alt and not out_alt:
-        modifiers.append("alt")
-    if in_super and not out_super:
-        modifiers.append("super")
-    if in_shift and not out_shift:
-        modifiers.append("shift")
-    command.extend(["keyup", "--delay", "0", "+".join(modifiers)])
+    modifiers = []
 
-if key_command:
-    command.extend(["key", "--delay", "0"])
-    if not clear_modifiers_manually:
-        command.append("--clearmodifiers")
-    command.append("+".join(key_command))
+    if clear_modifiers:
+        if in_ctrl and not out_ctrl:
+            modifiers.append("ctrl")
+        if in_alt and not out_alt:
+            modifiers.append("alt")
+        if in_super and not out_super:
+            modifiers.append("super")
+        if in_shift and not out_shift:
+            modifiers.append("shift")
 
-if click_key_command:
-    command.extend(["keydown", "--delay", "0", "+".join(click_key_command)])
+    if keyup_pre_command:
+        command.extend(make_xte_commands("keyup", keyup_pre_command, True))
 
-if click_command:
-    command.extend(["click", "--delay", "0", "+".join(click_command)])
+    if mouseup_pre_command:
+        command.extend(make_xte_commands("mouseup", [mouseup_pre_command]))
 
-if click_key_command:
-    command.extend(["keyup", "--delay", "0", "+".join(click_key_command)])
+    command.extend(make_xte_commands("keyup", modifiers, True))
 
-if clear_modifiers_manually:
-    command.extend(["keydown", "--delay", "0", "+".join(modifiers)])
+    if key_command:
+        command.extend(make_xte_commands("key", key_command))
+
+    if click_key_command:
+        command.extend(make_xte_commands("keydown", click_key_command))
+
+    if click_command:
+        command.extend(make_xte_commands("mouseclick", [click_command]))
+
+    if click_key_command:
+        command.extend(make_xte_commands("keyup", click_key_command))
+
+    command.extend(make_xte_commands("keydown", modifiers))
+
+else:
+
+    if keyup_pre_command:
+        command.extend(["keyup", "--delay", "2", "+".join(keyup_pre_command)])
+
+    if mouseup_pre_command:
+        # unrecognized option '--delay'
+        command.extend(["mouseup", mouseup_pre_command])
+
+    if key_command:
+        command.extend(["key", "--delay", "2"])
+        if clear_modifiers:
+            command.append("--clearmodifiers")
+        command.append("+".join(key_command))
+
+    if click_key_command:
+        command.extend(["keydown", "--delay", "2",
+                        "+".join(click_key_command)])
+
+    if click_command:
+        command.extend(["click", "--delay", "2", "+".join(click_command)])
+
+    if click_key_command:
+        command.extend(["keyup", "--delay", "2", "+".join(click_key_command)])
 
 if len(command) > 1:
     subprocess.run(command, check=True)
-
-logging.info("xdotool command: {0}".format(command))
+    logging.info("command: {0}".format(command))
+else:
+    logging.info("no command run")
