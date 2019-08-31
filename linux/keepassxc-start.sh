a04d3cbd0b7c754f4b42654d35f37add13fd8311
Luke Arms <luke@linacreative.com>
2019-08-31 15:01:26 +1000

    Add support for multiple databases to keepassxc-start.sh

--- linux/keepassxc-start.sh
+++ linux/keepassxc-start.sh
@@ -13,44 +13,27 @@ assert_not_root
 assert_command_exists keepassxc
 assert_command_exists secret-tool
 
-USAGE="Usage: $(basename "$0") </path/to/database> [/path/to/key_file]"
+USAGE="Usage: $(basename "$0") </path/to/database> [/path/to/another/database...]"
 
-if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+[ "$#" -gt "0" ] || die "$USAGE"
 
-    die "$USAGE"
+PASSWORDS=()
 
-fi
+for DATABASE_PATH in "$@"; do
 
-DATABASE_PATH="$1"
-KEY_PATH="${2:-}"
+    [ -f "$DATABASE_PATH" ] || die "$USAGE"
 
-[ -f "$DATABASE_PATH" ] || die "$USAGE"
+    if ! PASSWORD="$(secret-tool lookup "$DATABASE_PATH" keepassxc-password)"; then
 
-KEEPASSXC_OPTIONS=()
+        echoc "No password for ${BOLD}${DATABASE_PATH}${RESET} found in keyring. Please provide it now."
+        secret-tool store --label="KeePassXC password for $DATABASE_PATH" "$DATABASE_PATH" keepassxc-password
+        PASSWORD="$(secret-tool lookup "$DATABASE_PATH" keepassxc-password)" || PASSWORD=
 
-if [ -n "$KEY_PATH" ]; then
+    fi
 
-    [ -f "$KEY_PATH" ] || die "$USAGE"
+    PASSWORDS+=("$PASSWORD")
 
-    KEEPASSXC_OPTIONS+=(--keyfile "$KEY_PATH")
+done
 
-fi
-
-if ! KEEPASSXC_PASSWORD="$(secret-tool lookup "$DATABASE_PATH" keepassxc-password)"; then
-
-    echoc "No password for ${BOLD}${DATABASE_PATH}${RESET} found in keyring. Please provide it now."
-    secret-tool store --label="KeePassXC password for $DATABASE_PATH" "$DATABASE_PATH" keepassxc-password
-    KEEPASSXC_PASSWORD="$(secret-tool lookup "$DATABASE_PATH" keepassxc-password)" || KEEPASSXC_PASSWORD=
-
-fi
-
-if [ -n "$KEEPASSXC_PASSWORD" ]; then
-
-    KEEPASSXC_OPTIONS+=(--pw-stdin)
-    echo "$KEEPASSXC_PASSWORD" | keepassxc "${KEEPASSXC_OPTIONS[@]}" "$DATABASE_PATH"
-
-else
-
-    keepassxc "${KEEPASSXC_OPTIONS[@]}" "$DATABASE_PATH"
-
-fi
+IFS=$'\n\n\n'
+nohup keepassxc --pw-stdin "$@" >/dev/null 2>&1 <<<"${PASSWORDS[*]}" &
