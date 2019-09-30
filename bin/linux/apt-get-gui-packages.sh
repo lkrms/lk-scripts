64893210cb8130879c730239bc7e94f171ed5845
Luke Arms <luke@linacreative.com>
2019-09-30 11:27:54 +1000

    Move and rename apt-gui-packages.sh

--- bin/linux/apt-get-gui-packages.sh
+++ bin/linux/apt-get-gui-packages.sh
@@ -12,11 +12,11 @@ SCRIPT_PATH="${BASH_SOURCE[0]}"
 if command -v realpath >/dev/null 2>&1; then SCRIPT_PATH="$(realpath "$SCRIPT_PATH")"; fi
 SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd -P)"
 
-# shellcheck source=../bash/common
-. "$SCRIPT_DIR/../bash/common"
+# shellcheck source=../../bash/common
+. "$SCRIPT_DIR/../../bash/common"
 
-# shellcheck source=../bash/common-apt
-. "$SCRIPT_DIR/../bash/common-apt"
+# shellcheck source=../../bash/common-apt
+. "$SCRIPT_DIR/../../bash/common-apt"
 
 apt_refresh_packages
 
