c15e94b0cdee7469e90899032e22395ac963efd3
Luke Arms <luke@linacreative.com>
2019-10-03 09:28:56 +1000

    Add homebrew-install.sh

---
+++ bin/homebrew-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+if command -v realpath >/dev/null 2>&1; then SCRIPT_PATH="$(realpath "$SCRIPT_PATH")"; fi
+SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd -P)"
+
+# shellcheck source=../bash/common
+. "$SCRIPT_DIR/../bash/common"
+
+# shellcheck source=../bash/common-homebrew
+. "$SCRIPT_DIR/../bash/common-homebrew"
+
+assert_not_root
+
+brew_check
