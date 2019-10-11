44f52e2343925d1f59758bc3ea4ca3221c26cbbb
Luke Arms <luke@linacreative.com>
2019-10-11 20:52:29 +1100

    Add APT proxy reachability test

---
+++ bin/linux/apt-check-sources.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+if command -v realpath >/dev/null 2>&1; then SCRIPT_PATH="$(realpath "$SCRIPT_PATH")"; fi
+SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd -P)"
+
+# shellcheck source=../../bash/common
+. "$SCRIPT_DIR/../../bash/common"
+
+# shellcheck source=../../bash/common-apt
+. "$SCRIPT_DIR/../../bash/common-apt"
+
+apt_check_sources
