edf39e9e699a1f3341bb5c676fe584c3286f9b3f
Luke Arms <luke@linacreative.com>
2019-10-15 15:58:33 +1100

    Add cat-log-files

---
+++ bin/cat-log-files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+
+set -euo pipefail
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}" 2>/dev/null)" || SCRIPT_PATH="$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+. "$SCRIPT_DIR/../bash/common"
+
+[ "$#" -gt "0" ] && are_files "$@" || die "Usage: $(basename "$0") /path/to/file[.gz]..."
+
+# sort input files by modified date
+while IFS= read -rd $'\0' FILENAME; do
+
+    # print them to stdout, decompressing on the fly as needed
+    case "$FILENAME" in
+
+    *.gz)
+        gunzip <"$FILENAME"
+        ;;
+
+    *)
+        cat "$FILENAME"
+        ;;
+
+    esac
+
+done < <(gnu_stat --printf '%Y :%n\0' "$@" | sort -zn | cut -zd: -f2-)
