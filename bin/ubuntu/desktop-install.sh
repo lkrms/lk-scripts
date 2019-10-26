91e79cd604b029ce15cd3e4d941fdfa75dbd6840
Luke Arms <luke@linacreative.com>
2019-10-27 01:46:01 +1100

    Fix paths in desktop-install.sh

--- bin/ubuntu/desktop-install.sh
+++ bin/ubuntu/desktop-install.sh
@@ -5,9 +5,9 @@ set -euo pipefail
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}" 2>/dev/null)" || SCRIPT_PATH="$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
-. "$SCRIPT_DIR/../bash/common"
-. "$SCRIPT_DIR/../bash/common-apt"
-. "$SCRIPT_DIR/../bash/common-dev"
+. "$SCRIPT_DIR/../../bash/common"
+. "$SCRIPT_DIR/../../bash/common-apt"
+. "$SCRIPT_DIR/../../bash/common-dev"
 
 assert_is_ubuntu
 assert_has_gui
