9b7e9015f999d2d2b40d990e7522d768981e8a4b
Luke Arms <luke@linacreative.com>
2019-11-17 22:31:02 +1100

    Improve renaming when adding new episodes

--- bin/file-rename-episodes.sh
+++ bin/file-rename-episodes.sh
@@ -48,21 +48,29 @@ while read -rd $'\0' FILE_PATH; do
 
     fi
 
-    ((++COUNT))
-
-    PADDED_COUNT="$(printf "%02d" "$COUNT")"
-
     FILE_NAME="$(basename "$FILE_PATH")"
-
-    [[ "$FILE_NAME" =~ [^0-9]"$PADDED_COUNT"[^0-9] ]] || console_message "WARNING: this doesn't look like an episode $COUNT:" "$FILE_PATH" "$BOLD" "$RED"
-
     FILE_EXT="${FILE_PATH##*.}"
     [ "$FILE_EXT" != "mp4" ] || FILE_EXT="m4v"
 
-    NEW_FILENAME="${SERIES_NAME}${SEASON_NAME}_E${PADDED_COUNT}.${FILE_EXT}"
+    ! [[ "${FILE_NAME#${SERIES_NAME}${SEASON_NAME}_E}" =~ ^[0-9]{2}\."$FILE_EXT"$ ]] || {
+        console_message "Skipping (already renamed):" "$FILE_PATH" "$BOLD" "$RED"
+        continue
+    }
 
-    [ "$NEW_FILENAME" != "$(basename "$FILE_PATH")" ] || continue
+    while :; do
 
-    maybe_dryrun mv -vn "$FILE_PATH" "$(dirname "$FILE_PATH")/$NEW_FILENAME" || die
+        ((++COUNT))
+        PADDED_COUNT="$(printf "%02d" "$COUNT")"
+
+        NEW_FILENAME="${SERIES_NAME}${SEASON_NAME}_E${PADDED_COUNT}.${FILE_EXT}"
+        NEW_PATH="$(dirname "$FILE_PATH")/$NEW_FILENAME"
+
+        [ -e "$NEW_PATH" ] || break
+
+    done
+
+    [[ "$FILE_NAME" =~ [^0-9]"$COUNT"[^0-9] ]] || console_message "WARNING: this doesn't look like an episode $COUNT:" "$FILE_PATH" "$BOLD" "$RED"
+
+    maybe_dryrun mv -vn "$FILE_PATH" "$NEW_PATH" || die
 
 done < <(find "$RENAME_ROOT" -type f \( -name '*.m4v' -o -name '*.mkv' -o -name '*.mp4' \) ! -name '.*' -print0 | sort -z)
