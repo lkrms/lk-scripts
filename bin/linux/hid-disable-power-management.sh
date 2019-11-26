e3484257675b8700572a3fae01212c55563177cf
Luke Arms <luke@linacreative.com>
2019-11-26 12:20:42 +1100

    Improve hid-disable-power-management.sh

--- bin/linux/hid-disable-power-management.sh
+++ bin/linux/hid-disable-power-management.sh
@@ -10,33 +10,78 @@ SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
 shopt -s nullglob
 
-for DEVICE in $(printf '%s\n' /sys/bus/usb/drivers/usbhid/* | gnu_grep -Po '(?<=/)[-0-9\.]+(?=:[0-9\.]+$)' | sort | uniq); do
+DIRS=("$@")
+[ "$#" -gt "0" ] || DIRS=("/sys/bus/usb/drivers/usb")
+USAGE="Usage: $(basename "$0") [/path/to/sys/device/root | /path/to/sys/device...]"
 
-    PRODUCT="$(<"/sys/bus/usb/devices/$DEVICE/product")"
-    CURRENT_STATUS="$(<"/sys/bus/usb/devices/$DEVICE/power/control")"
+DEVICES=()
+
+for d in "${DIRS[@]}"; do
+
+    [ ! -e "$d/power/control" ] || DEVICES+=("$(realpath "$d")")
+
+    if [ -d "$d" ]; then
+
+        IFS=$'\n'
+        DEVICES+=($(find -L "$d" -type d ! \( -regextype posix-extended -regex '.*/[0-9]+-[0-9]+(.[0-9]+)*:[0-9]+.[0-9]+' -prune \) -exec test -f '{}/power/control' \; -prune -exec realpath '{}' \; 2>/dev/null))
+        unset IFS
+
+    fi
+
+done
+
+[ "${#DEVICES[@]}" -gt "0" ] || die "$USAGE"
+
+IFS=$'\n'
+DEVICES=($(printf "%s\n" "${DEVICES[@]}" | sort | uniq))
+unset IFS
+
+KERNEL_MANUFACTURER="$(uname -sr)"
+
+for DEVICE in "${DEVICES[@]}"; do
+
+    PRODUCT="${DEVICE#/sys/devices/}"
+    CURRENT_STATUS="$(<"$DEVICE/power/control")"
+
+    [ ! -f "$DEVICE/idVendor" ] && [ ! -f "$DEVICE/idProduct" ] || PRODUCT="$(<"$DEVICE/idVendor"):$(<"$DEVICE/idProduct") at $PRODUCT"
+
+    # attempt to get a human-readable product name
+    PRODUCT_NAME=()
+    [ ! -f "$DEVICE/manufacturer" ] || grep -Fq "$KERNEL_MANUFACTURER" "$DEVICE/manufacturer" || PRODUCT_NAME+=("$(<"$DEVICE/manufacturer")")
+    [ ! -f "$DEVICE/product" ] || PRODUCT_NAME+=("$(<"$DEVICE/product")")
+    [ "${#PRODUCT_NAME[@]}" -eq "0" ] && PRODUCT="${CYAN}${PRODUCT}${RESET}" || PRODUCT="${BOLD}${CYAN}${PRODUCT_NAME[*]}${RESET}${GREY} (${PRODUCT})${RESET}"
 
     case "$(basename "$0")" in
 
     *get*)
 
-        [ "$CURRENT_STATUS" = "on" ] && CURRENT_STATUS="disabled" || CURRENT_STATUS="enabled"
-        printf "Power management for ${BOLD}%s${RESET} is currently ${BOLD}%s${RESET}\n" "$PRODUCT" "$CURRENT_STATUS"
+        [ "$CURRENT_STATUS" = "on" ] &&
+            {
+                PM_STATUS="disabled"
+                PM_COLOUR="$RED"
+            } ||
+            {
+                PM_STATUS="enabled"
+                PM_COLOUR="$GREEN"
+            }
+
+        printf "Power management for %s is currently ${BOLD}${PM_COLOUR}%s${RESET}\n" "$PRODUCT" "$PM_STATUS"
         ;;
 
     *disable*)
 
         [ "$CURRENT_STATUS" != "on" ] || {
-            printf "Power management already disabled for ${BOLD}%s${RESET}\n" "$PRODUCT"
+            printf "Power management already disabled for %s\n" "$PRODUCT"
             continue
         }
 
-        if sudo tee "/sys/bus/usb/devices/$DEVICE/power/control" >/dev/null <<<"on"; then
+        if sudo tee "$DEVICE/power/control" >/dev/null <<<"on"; then
 
-            printf "Power management disabled for ${BOLD}%s${RESET}\n" "$PRODUCT"
+            printf "Power management disabled for %s\n" "$PRODUCT"
 
         else
 
-            printf "Error disabling power management for ${BOLD}%s${RESET}\n" "$PRODUCT"
+            printf "Error disabling power management for %s\n" "$PRODUCT"
 
         fi
         ;;
@@ -44,17 +89,17 @@ for DEVICE in $(printf '%s\n' /sys/bus/usb/drivers/usbhid/* | gnu_grep -Po '(?<=
     *enable*)
 
         [ "$CURRENT_STATUS" = "on" ] || {
-            printf "Power management already enabled for ${BOLD}%s${RESET}\n" "$PRODUCT"
+            printf "Power management already enabled for %s\n" "$PRODUCT"
             continue
         }
 
-        if sudo tee "/sys/bus/usb/devices/$DEVICE/power/control" >/dev/null <<<"auto"; then
+        if sudo tee "$DEVICE/power/control" >/dev/null <<<"auto"; then
 
-            printf "Power management enabled for ${BOLD}%s${RESET}\n" "$PRODUCT"
+            printf "Power management enabled for %s\n" "$PRODUCT"
 
         else
 
-            printf "Error enabling power management for ${BOLD}%s${RESET}\n" "$PRODUCT"
+            printf "Error enabling power management for %s\n" "$PRODUCT"
 
         fi
         ;;
