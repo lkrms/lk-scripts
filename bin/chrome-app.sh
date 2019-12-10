99618ca32b2d43d591f2eadf74ad46db676d5a29
Luke Arms <luke@linacreative.com>
2019-12-11 00:48:17 +1100

    Add chrome-app.sh (and harvest.desktop as first implementation)

---
+++ bin/chrome-app.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+
+set -euo pipefail
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}" 2>/dev/null)" || SCRIPT_PATH="$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "${BASH_SOURCE[0]}")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+. "$SCRIPT_DIR/../bash/common"
+
+assert_not_root
+
+[ "$#" -eq "2" ] || die "Usage: $(basename "$0") url instancename [chrome_arg ...]"
+
+URL="$1"
+INSTANCE_NAME="$2"
+shift 2
+
+[ -d "${HOME:-}" ] || die "HOME not set"
+
+google-chrome --user-data-dir="$HOME/.config/$INSTANCE_NAME" --no-first-run --app="$URL" "$@"
