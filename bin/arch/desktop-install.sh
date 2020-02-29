48b075075df81ea8047d8f857a93b4ad525caac4
Luke Arms <luke@linacreative.com>
2020-03-01 01:42:13 +1100

    Add more packages and AUR function

--- bin/arch/desktop-install.sh
+++ bin/arch/desktop-install.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-# shellcheck disable=SC1090,SC2034
+# shellcheck disable=SC1090
 
 set -euo pipefail
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}" 2>/dev/null)" || SCRIPT_PATH="$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+
+. "$SCRIPT_DIR/../../bash/common"
+
+function lk_install_aur() {
+    local TEMP_DIR
+    TEMP_DIR="$(lk_mktemp_dir)" || return
+    lk_delete_on_exit "$TEMP_DIR"
+    (
+        cd "$TEMP_DIR" &&
+            git clone "https://aur.archlinux.org/$1.git" &&
+            cd "$1" &&
+            makepkg -si
+    )
+}
 
 INSTALL=()
 
@@ -61,6 +75,7 @@ INSTALL+=(
     flameshot
     geany
     guake
+    keepassxc
     libreoffice-fresh
     nextcloud-client
     recoll
@@ -75,6 +90,7 @@ INSTALL+=(
     code
     dbeaver
     git
+    java-openjdk
 )
 
 # libvirt
@@ -93,3 +109,5 @@ xfconf-query -c xfwm4 -p /general/theme -n -t string -s "Arc-Dark-solid"
 xfconf-query -c xsettings -p /Net/IconThemeName -n -t string -s "Papirus-Dark"
 xfconf-query -c xsettings -p /Net/SoundThemeName -n -t string -s "elementary"
 xfconf-query -c xsettings -p /Net/ThemeName -n -t string -s "Arc-Dark-solid"
+
+lk_command_exists yay || lk_install_aur yay
