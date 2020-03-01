89d214957aab33f0c3e114209c00c6380e55f516
Luke Arms <luke@linacreative.com>
2020-03-01 16:38:24 +1100

    Fix offer_sudo_password_bypass and add Arch packages

--- bin/arch/desktop-install.sh
+++ bin/arch/desktop-install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# shellcheck disable=SC1090
+# shellcheck disable=SC1090,SC2034
 
 set -euo pipefail
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}" 2>/dev/null)" || SCRIPT_PATH="$(python -c 'import os,sys;print os.path.realpath(sys.argv[1])' "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
 . "$SCRIPT_DIR/../../bash/common"
+
+assert_is_desktop
+assert_not_root
 
 function lk_install_aur() (
     ERRORS=()
@@ -27,18 +30,171 @@ function lk_install_aur() (
         fi
         makepkg -si --noconfirm --needed || ERRORS+=("$AUR")
     done
-    [ "${#ERRORS[@]}" -eq "0" ] || lk_echo_array "${ERRORS[@]}" || lk_console_list "Failed to install" "AUR package" "AUR packages" "$BOLD$RED"
+    [ "${#ERRORS[@]}" -eq "0" ] || lk_echo_array "${ERRORS[@]}" | lk_console_list "Failed to install" "AUR package" "AUR packages" "$BOLD$RED"
 )
 
 PAC_INSTALL=()
 AUR_INSTALL=()
 
+# alis.sh has these covered
+PAC_PRE_INSTALLED=(
+    linux
+    linux-headers
+    networkmanager
+    xdg-user-dirs
+
+    # probably
+    dosfstools
+    grub
+    intel-media-driver
+    intel-ucode
+    lightdm
+    lightdm-gtk-greeter
+    linux-zen
+    linux-zen-headers
+    mesa
+    vulkan-icd-loader
+    vulkan-intel
+    xfce4
+    xfce4-goodies
+    xorg-server
+
+    # probably not
+    lvm2
+
+    # if needed
+    virtualbox-guest-dkms
+    virtualbox-guest-modules-arch
+    virtualbox-guest-utils
+    virtualbox-guest-utils
+
+    # added in lkrms/alis
+    b43-fwcutter
+    btrfs-progs
+    crda
+    curl
+    dhclient
+    dhcpcd
+    diffutils
+    dmidecode
+    dmraid
+    dnsmasq
+    dosfstools
+    ethtool
+    exfat-utils
+    f2fs-tools
+    gnome-initial-setup
+    gnu-netcat
+    gpm
+    gptfdisk
+    grub
+    hdparm
+    ipw2100-fw
+    ipw2200-fw
+    jfsutils
+    lftp
+    linux-firmware
+    lsb-release
+    lvm2
+    man-db
+    man-pages
+    mdadm
+    mtools
+    nano
+    net-tools
+    network-manager-applet
+    nfs-utils
+    nmap
+    ntfs-3g
+    ntp
+    openconnect
+    openssh
+    openvpn
+    parted
+    ppp
+    pptpclient
+    reiserfsprogs
+    rsync
+    smartmontools
+    sudo
+    tcpdump
+    usb_modeswitch
+    usbutils
+    vi
+    vpnc
+    wget
+    wireless_tools
+    wireless-regdb
+    wpa_supplicant
+    xfsprogs
+    xl2tpd
+
+    # "base"
+    bash
+    bzip2
+    coreutils
+    file
+    filesystem
+    findutils
+    gawk
+    gcc-libs
+    gettext
+    glibc
+    grep
+    gzip
+    iproute2
+    iputils
+    licenses
+    pacman
+    pciutils
+    procps-ng
+    psmisc
+    sed
+    shadow
+    systemd
+    systemd-sysvcompat
+    tar
+    util-linux
+    xz
+
+    # "base-devel"
+    autoconf
+    automake
+    binutils
+    bison
+    fakeroot
+    file
+    findutils
+    flex
+    gawk
+    gcc
+    gettext
+    grep
+    groff
+    gzip
+    libtool
+    m4
+    make
+    pacman
+    patch
+    pkgconf
+    sed
+    sudo
+    texinfo
+    which
+)
+
 # hardware-related
 is_virtual || PAC_INSTALL+=(
-    blueman
-    pulseaudio-bluetooth
+    hddtemp
+    lm_sensors
+    powertop
     tlp
     tlp-rdw
+
+    # desktop-only
+    blueman
+    pulseaudio-bluetooth
 )
 
 # essentials
@@ -81,10 +237,39 @@ PAC_INSTALL+=(
 
 # terminal-based
 PAC_INSTALL+=(
+    # shells
+    bash-completion
+    byobu
+    ksh
+    zsh
+
+    # utilities
+    bc
+    mediainfo
     p7zip
-    trash-cli
+    pv
+    stow
     unison
     vim
+
+    # networking
+    bridge-utils
+    traceroute
+    whois
+
+    # monitoring
+    atop
+    glances
+    htop
+    iftop
+    iotop
+    nethogs
+    nload
+    sysstat
+
+    # system
+    hwinfo
+    sysfsutils
 )
 
 # desktop
@@ -107,6 +292,7 @@ PAC_INSTALL+=(
     scribus
     speedcrunch
     thunderbird
+    trash-cli
 )
 
 AUR_INSTALL+=(
@@ -136,7 +322,6 @@ AUR_INSTALL+=(
 
 # libvirt
 PAC_INSTALL+=(
-    bridge-utils
     dnsmasq
     ebtables
     libvirt
