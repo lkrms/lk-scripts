9a405321da31bf1f37f8dffea296fa2c31daa371
Luke Arms <luke@linacreative.com>
2020-04-24 17:56:33 +1000

    Work around wide-dhcpv6-client failure if ppp interface is down

--- bin/linux/lk_ppp_up.sh
+++ bin/linux/lk_ppp_up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# symlink in /etc/ppp/ip-up.d if needed
+# Disable wide-dhcpv6-client.service and add a symlink to this script
+# in /etc/ppp/ip-up.d if needed.
 
 [ -n "$PPP_IFACE" ] &&
     [ -d "/proc/sys/net/ipv6/conf/$PPP_IFACE" ] || exit 0
@@ -9,6 +10,10 @@ case "$(basename "$0")" in
 
 *up*)
     echo 2 >"/proc/sys/net/ipv6/conf/$PPP_IFACE/accept_ra" || :
+    # start /usr/sbin/dhcp6c if it's not already running
+    [ -x "/etc/init.d/wide-dhcpv6-client" ] &&
+        /etc/init.d/wide-dhcpv6-client status ||
+        /etc/init.d/wide-dhcpv6-client start || :
     ;;
 
 *down*)
