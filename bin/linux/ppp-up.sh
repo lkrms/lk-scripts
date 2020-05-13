d5b547f9d143a325a1c45a663a58993901ae3f0d
Luke Arms <luke@linacreative.com>
2020-05-13 10:49:16 +1000

    Add sqm-based shaping to ppp-up.sh

--- bin/linux/ppp-up.sh
+++ bin/linux/ppp-up.sh
@@ -10,15 +10,34 @@ case "$(basename "$0")" in
 
 *up*)
     echo 2 >"/proc/sys/net/ipv6/conf/$PPP_IFACE/accept_ra" || :
-    # start /usr/sbin/dhcp6c if it's not already running
-    [ -x "/etc/init.d/wide-dhcpv6-client" ] &&
-        /etc/init.d/wide-dhcpv6-client status ||
+    # [re]start /usr/sbin/dhcp6c
+    if [ -x "/etc/init.d/wide-dhcpv6-client" ]; then
+        /etc/init.d/wide-dhcpv6-client status &&
+            /etc/init.d/wide-dhcpv6-client stop || :
         /etc/init.d/wide-dhcpv6-client start || :
+    fi
+    # start traffic shaping (see https://github.com/tohojo/sqm-scripts)
+    if [ -x "/usr/bin/sqm" ] &&
+        [ -f "/etc/sqm/sqm.conf" ]; then
+        . "/etc/sqm/sqm.conf"
+        if [ -n "$SQM_STATE_DIR" ]; then
+            [ -e "$SQM_STATE_DIR/$PPP_IFACE.state" ] &&
+                /usr/bin/sqm reload "$PPP_IFACE" ||
+                /usr/bin/sqm start "$PPP_IFACE" || :
+        fi
+    fi
     ;;
 
 *down*)
-    # nothing to do
-    :
+    # stop traffic shaping
+    if [ -x "/usr/bin/sqm" ] &&
+        [ -f "/etc/sqm/sqm.conf" ]; then
+        . "/etc/sqm/sqm.conf"
+        if [ -n "$SQM_STATE_DIR" ]; then
+            [ ! -e "$SQM_STATE_DIR/$PPP_IFACE.state" ] ||
+                /usr/bin/sqm stop "$PPP_IFACE" || :
+        fi
+    fi
     ;;
 
 esac
